f4f890df87b9099d9ed64c629f2c62ae1a74d301
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-08-26 14:24:50 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'tommy@fuckface.com',
-        text: 'this is from the client to the server'
-    });
 });
 
 socket.on('disconnect', function () {
