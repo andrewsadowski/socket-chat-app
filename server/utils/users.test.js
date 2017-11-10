601c9b4335dd77ca9a75c4e694080ca0c872f340
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-11-09 18:08:29 -0800

    Updated User class and added more test cases

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -4,6 +4,25 @@ const {Users} = require('./users');
 
 describe('Users', () => {
     
+    var users;
+    beforeEach(() => {
+        users = new Users();
+        users.users = [{
+            id: '1',
+            name: 'Mike',
+            room: 'Secret Room'
+        }, {
+            id: '2',
+            name: 'Jen',
+            room: 'Super Secret Room'
+        }, {
+            id: '3',
+            name: 'Joan',
+            room: 'Secret Room'
+        }];
+    });
+
+
     it('should add new user', () => {
         var users = new Users();
         var user = {
@@ -15,4 +34,9 @@ describe('Users', () => {
 
         expect(users.users).toEqual([user]);
     });
+
+    it('should return names for Secret Room', () => {
+        var userList = users.getUserList('Secret Room');
+        expect(userList).toEqual(['Mike', 'Joan']);
+    })
 });
