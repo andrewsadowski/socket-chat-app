accdd8da5d02b60e83368477df01a62a728e0835
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-11-09 18:59:40 -0800

    User class with getUser, removeUser, and getUserList with test cases

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -38,5 +38,33 @@ describe('Users', () => {
     it('should return names for Secret Room', () => {
         var userList = users.getUserList('Secret Room');
         expect(userList).toEqual(['Mike', 'Joan']);
-    })
+    });
+
+    it('should remove a user', () => {
+        var userId = '1';
+        var user = users.removeUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    });
+
+    it('should not remove user', () => {
+        var userId = '99';
+        var user = users.removeUser(userId);
+
+        expect(user).toNotExist();
+        expect(users.users.length).toBe(3);
+    });
+
+    it('should find user', () => {
+        var userId = '2';
+        var user = users.getUser(userId);
+        expect(user.id).toBe(userId);
+    });
+
+    it('should not find user', () => {
+        var userId = '99';
+        var user = users.getUser(userId);
+        expect(user).toNotExist();
+    });
 });
